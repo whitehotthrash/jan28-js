f504cbe81cb2005692dfcd32ea7aa8ca6aadd281
Tim <tim.maastricht@gmail.com>
2026-01-28 18:50:02 +1000

    feat: Add content to the dom

---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log("JavaScript has loaded!");
+
+let mainContainer = document.getElementById("mainContainer");
+let testimonial = document.createElement("div");
+
+mainContainer.appendChild(testimonial);
