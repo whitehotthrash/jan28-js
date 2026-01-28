f69e602e1685ad9fa326d220a41cbb9cb7a097ee
Tim <tim.maastricht@gmail.com>
2026-01-28 19:44:18 +1000

    feat: add event listener

--- src/index.js
+++ src/index.js
@@ -4,3 +4,24 @@ let mainContainer = document.getElementById("mainContainer");
 let testimonial = document.createElement("div");
 
 mainContainer.appendChild(testimonial);
+
+// remove content
+
+//let targetToRemove = document.getElementsByClassName("mainContainer")[0];
+// targetToRemove.remove();
+//mainContainer.removeChild(testimonial);
+
+window.addEventListener("keydown", (eventData) => {
+  // when key is pressed, do stuff
+  if (eventData.code === "Space"){
+    alert("Space has been pressed!");
+    return
+  }
+})
+
+document.getElementById("mainContainer").addEventListener("keydown", (eventData) => {
+  if (eventData.key === " "){
+    console.log("Space has been pressed");
+    return
+  }
+})
