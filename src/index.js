5dbf2cb70a0732021a8e59e84d88ca6d86e59208
Tim <tim.maastricht@gmail.com>
2026-02-01 14:20:07 +1000

    feat: add callback function with timer

--- src/index.js
+++ src/index.js
@@ -13,25 +13,38 @@ mainContainer.appendChild(testimonial);
 
 window.addEventListener("keydown", (eventData) => {
   // when key is pressed, do stuff
-  if (eventData.code === "Space"){
+  if (eventData.code === "Space") {
     console.log("Space has been pressed!");
-    return
+    return;
   }
   if (eventData.code === "Meta") {
     console.log("Meta key has been pressed");
-    return
+    return;
   }
-})
-
-const button = document.getElementById('change-color-button');
-
-const elementToChange = document.getElementById('color-changeable-element');
-
-button.addEventListener('click', function() {
-
-  elementToChange.style.backgroundColor = 'red';
-
 });
+
+const button = document.getElementById("change-color-button");
+
+const elementToChange = document.getElementById("color-changeable-element");
+
+button.addEventListener("click", function () {
+  elementToChange.style.backgroundColor = "red";
+});
+
+// callback:
+
+function greet(name, callback) {
+  console.log(`Hello, ${name}`);
+  callback();
+}
+
+function sayGoodbye() {
+  setTimeout(() => {
+    console.log("Goodbye!");
+  }, 3000);
+}
+
+greet("sam", sayGoodbye);
 
 // document.getElementById("mainContainer").addEventListener("keydown", (eventData) => {
 //   if (eventData.key === " "){
